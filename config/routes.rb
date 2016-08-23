a654cc55927addf93b242e946827c071b11e8240
Kyrene Jackson <ky.jackson01@gmail.com>
2016-08-22 22:54:01 -0500

    User can create and view posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'posts/index'
+  root 'posts#index'
+
+  get '/posts/new' => 'posts#new'
+  post '/posts' => 'posts#create'
+  get '/posts/:id' => 'posts#show', as: 'post'
+
 end
